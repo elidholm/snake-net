0225f128bb065b3f4a4afdfeb5d829928e20908c
Edvin Lidholm <edvin.lidholm@volvocars.com>
2024-09-24 16:36:27 +0200

    test: add pytest support for unit tests of layer module

---
+++ tests/test_layer.py
@@ -0,0 +1,74 @@
+"""Test the Layer_Dense class."""
+
+import numpy as np
+from snake_net.layer import Layer_Dense
+import pytest
+
+
+class TestLayerDense:
+
+    @pytest.fixture(scope="function")
+    def first_layer(self) -> Layer_Dense:
+        return Layer_Dense(4, 5)
+
+    @pytest.fixture(scope="function")
+    def second_layer(self) -> Layer_Dense:
+        return Layer_Dense(5, 2)
+
+    @pytest.fixture(scope="function")
+    def input(self) -> np.ndarray:
+        input = np.array(
+            [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+        )
+        return input
+
+    def test_one_layer_output_shape(self, first_layer: Layer_Dense, input: np.ndarray):
+        np.random.seed(0)
+
+        first_layer.forward(input)
+        actual = first_layer.output.shape
+        expected = (3, 5)
+        assert actual == expected
+
+    def test_two_layers_output_shape(
+        self, first_layer: Layer_Dense, second_layer: Layer_Dense, input: np.ndarray
+    ):
+        np.random.seed(0)
+
+        first_layer.forward(input)
+        second_layer.forward(first_layer.output)
+        actual = second_layer.output.shape
+        expected = (3, 2)
+        assert actual == expected
+
+    def test_one_layer_output_values(self, first_layer: Layer_Dense, input: np.ndarray):
+        np.random.seed(0)
+
+        first_layer.forward(input)
+        actual = first_layer.output
+        expected = np.array(
+            [
+                [0.10758131, 1.03983522, 0.24462411, 0.31821498, 0.18851053],
+                [-0.08349796, 0.70846411, 0.00293357, 0.44701525, 0.36360538],
+                [-0.50763245, 0.55688422, 0.07987797, -0.34889573, 0.04553042],
+            ]
+        )
+        assert np.allclose(actual, expected)
+
+    def test_two_layers_output_values(
+        self, first_layer: Layer_Dense, second_layer: Layer_Dense, input: np.ndarray
+    ):
+        np.random.seed(0)
+
+        first_layer.forward(input)
+        second_layer.forward(first_layer.output)
+        actual = second_layer.output
+        print(actual)
+        expected = np.array(
+            [
+                [0.148296, -0.08397602],
+                [0.14100315, -0.01340469],
+                [0.20124979, -0.07290616],
+            ]
+        )
+        assert np.allclose(actual, expected)
