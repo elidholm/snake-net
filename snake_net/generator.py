fe14a270356cbe3d14ce8dc11d5f436e5f449044
Edvin Lidholm <edvin.lidholm@volvocars.com>
2024-09-27 11:39:56 +0200

    feat: add sample data generator with tests

---
+++ snake_net/generator.py
@@ -0,0 +1,44 @@
+"""Generate data for classification task"""
+
+import logging
+from functools import lru_cache
+
+import matplotlib.pyplot as plt
+import numpy as np
+from rich.logging import RichHandler
+
+logging.basicConfig(level=logging.INFO, format="%(message)s", datefmt="%Y-%m-%d %H:%M:%S", handlers=[RichHandler()])
+_log = logging.getLogger(__name__)
+
+
+class Generator:
+    @lru_cache(maxsize=128)
+    def create_data(self, n_points: int, n_classes: int):
+        points = np.zeros((n_points * n_classes, 2))
+        labels = np.zeros(n_points * n_classes, dtype="uint8")
+        for class_number in range(n_classes):
+            ix = range(n_points * class_number, n_points * (class_number + 1))
+            r = np.linspace(0.0, 1, n_points)
+            t = np.linspace(class_number * 4, (class_number + 1) * 4, n_points) + np.random.randn(n_points) * 0.2
+            points[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+            labels[ix] = class_number
+        return points, labels
+
+
+def main():
+    n_points, n_classes = 100, 3
+    _log.info("Number of points: %d", n_points)
+    _log.info("Number of classes: %d", n_classes)
+
+    data_generator = Generator()
+    points, labels = data_generator.create_data(n_points, n_classes)
+    _log.debug("Generated points:\n%s", points)
+    _log.debug("Labels:\n%s", labels)
+
+    plt.scatter(points[:, 0], points[:, 1], c=labels, cmap="brg")
+    plt.show()
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    main()
---
+++ tests/test_generator.py
@@ -0,0 +1,34 @@
+"""Sample data generator test."""
+
+import pytest
+
+from snake_net.generator import Generator
+
+
+class TestSampleDataGenerator:
+
+    @pytest.fixture(scope="function")
+    def data_generator(self) -> Generator:
+        return Generator()
+
+    @pytest.fixture(scope="function")
+    def n_points(self) -> int:
+        return 100
+
+    @pytest.fixture(scope="function")
+    def n_classes(self) -> int:
+        return 3
+
+    def test_shape_of_output_points_should_be_correct(self, data_generator: Generator, n_points: int, n_classes: int):
+        """Check that output points are the correct shape."""
+        points, _ = data_generator.create_data(n_points, n_classes)
+        actual = points.shape
+        expected = (n_points * n_classes, 2)
+        assert actual == expected
+
+    def test_shape_of_output_labels_should_be_correct(self, data_generator: Generator, n_points: int, n_classes: int):
+        """Check that outputs labels are the correct shape."""
+        _, labels = data_generator.create_data(n_points, n_classes)
+        actual = labels.shape
+        expected = (n_points * n_classes,)
+        assert actual == expected
